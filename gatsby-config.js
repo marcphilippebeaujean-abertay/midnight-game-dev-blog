65bced7b81edfdf54b4fc1fcda1d0e7e1339aac7
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-04-25 17:40:47 +0200

    added purgce css

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,17 @@ const plugins = [
             path: `${__dirname}/static/images`,
         },
     },
+    {
+        resolve: `gatsby-plugin-purgecss`,
+        options: {
+            printRejected: true, // Print removed selectors and processed file names
+            develop: true, // Enable while using `gatsby develop`
+            // tailwind: true, // Enable tailwindcss support
+            // whitelist: ['whitelist'], // Don't remove this selector
+            // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+            // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+        }
+    }
 ];
 
 if (siteMetadata.disqus) {
