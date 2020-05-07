0ebd5dd314809df42e49ad656ba598785ac0fb9d
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-05-07 10:55:45 +0200

    added google analytics hookup

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,7 +129,24 @@ const plugins = [
             // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
             // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
         }
-    }
+    },
+    {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+            // The property ID; the tracking code won't be generated without it
+            trackingId: "UA-153657097-3",
+            // Defines where to place the tracking script - `true` in the head and `false` in the body
+            head: false,
+            // Setting this parameter is optional
+            anonymize: true,
+            // Setting this parameter is also optional
+            respectDNT: true,
+            // Delays sending pageview hits on route update (in milliseconds)
+            pageTransitionDelay: 1000,
+            // Defers execution of google analytics script after page load
+            defer: true,
+        },
+    },
 ];
 
 if (siteMetadata.disqus) {
