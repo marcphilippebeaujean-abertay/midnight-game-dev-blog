091c4503c6ae9fab50d56848626e5606df5f620e
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-07-10 15:56:36 +0200

    added new game assets, added new part of jet island episodes, added new free assets

--- src/components/podcast-links.js
+++ src/components/podcast-links.js
@@ -11,47 +11,57 @@ const LinkButton = ({ iconImage, url, name }) => (
     </div>
 )
 
-export default () => {
+
+export const PodcastLinks = ({ title, applePodcastLink, googlePodcastLink, spotifyLink, rssLink, websiteLink }) => {
     const data = useStaticQuery(graphql`
-        query {
-            applePodcast: file(relativePath: { eq: "podcast_icons/apple_podcast.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 40) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            googlePodcast: file(relativePath: { eq: "podcast_icons/google-podcast.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 40) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            spotifyIcon: file(relativePath: { eq: "podcast_icons/spotify.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 40) {
-                        ...GatsbyImageSharpFluid
-                    }
-                }
-            }
-            rssIcon: file(relativePath: { eq: "podcast_icons/rss.png" }) {
-                childImageSharp {
-                    fluid(maxWidth: 40) {
-                        ...GatsbyImageSharpFluid
-                    }
+    query {
+        applePodcast: file(relativePath: { eq: "podcast_icons/apple_podcast.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 40) {
+                    ...GatsbyImageSharpFluid
                 }
             }
         }
-    `)
+        googlePodcast: file(relativePath: { eq: "podcast_icons/google-podcast.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 40) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        spotifyIcon: file(relativePath: { eq: "podcast_icons/spotify.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 40) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+        rssIcon: file(relativePath: { eq: "podcast_icons/rss.png" }) {
+            childImageSharp {
+                fluid(maxWidth: 40) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+    }
+`)
     return (
-        <div className="podcast-links social-links">
-            <LinkButton iconImage={data.applePodcast.childImageSharp.fluid} name={"Apple Podcast"} url={"https://podcasts.apple.com/de/podcast/moonlight-game-devs/id1511553688"} />
-            <LinkButton iconImage={data.googlePodcast.childImageSharp.fluid} name={"Google Podcast"} url={"https://podcasts.google.com/?feed=aHR0cHM6Ly9sZXRzY2FzdC5mbS9wb2RjYXN0cy9tb29ubGlnaHQtZ2FtZS1kZXZzLTEzNDViYTAxL2ZlZWQ&ep=14"} />
-            <LinkButton iconImage={data.spotifyIcon.childImageSharp.fluid} name={"Spotify"} url={"https://open.spotify.com/show/6aDR2KdCiwPoSySMphy2Ha?si=1Ct3IZSZRWCR4GwqGT_ltg"} />
-            <LinkButton iconImage={data.rssIcon.childImageSharp.fluid} name={"RSS Feed"} url={"https://letscast.fm/podcasts/moonlight-game-devs-1345ba01/feed"} />
+        <div>            {title && <h2 className="podcast-header">{title}</h2>}
+            <div className="podcast-links social-links">
+                {applePodcastLink && <LinkButton iconImage={data.applePodcast.childImageSharp.fluid} name={"Apple Podcast"} url={applePodcastLink} />}
+                {googlePodcastLink && <LinkButton iconImage={data.googlePodcast.childImageSharp.fluid} name={"Google Podcast"} url={googlePodcastLink} />}
+                {spotifyLink && <LinkButton iconImage={data.spotifyIcon.childImageSharp.fluid} name={"Spotify"} url={spotifyLink} />}
+                {rssLink &&
+                    <LinkButton iconImage={data.rssIcon.childImageSharp.fluid} name={"RSS Feed"} url={rssLink} />}
+            </div>
         </div>
+
     )
 }
+
+export default () => <PodcastLinks applePodcastLink={"https://podcasts.apple.com/de/podcast/moonlight-game-devs/id1511553688"}
+    googlePodcastLink={"https://podcasts.google.com/?feed=aHR0cHM6Ly9sZXRzY2FzdC5mbS9wb2RjYXN0cy9tb29ubGlnaHQtZ2FtZS1kZXZzLTEzNDViYTAxL2ZlZWQ&ep=14"}
+    spotifyLink={"https://open.spotify.com/show/6aDR2KdCiwPoSySMphy2Ha?si=1Ct3IZSZRWCR4GwqGT_ltg"}
+    rssLink={"https://letscast.fm/podcasts/moonlight-game-devs-1345ba01/feed"} />
 
 
